c87a80e1e59a0671bc2663b7e956bb24ea03e03f
JEgholm <109749520+JEgholm@users.noreply.github.com>
2025-01-31 14:56:52 +0100

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
+  base: '/Quiz-PWA/',  // Ensure assets are correctly loaded on GitHub Pages
   plugins: [vue()],
   resolve: {
     alias: {
